63dd94d82b6d63f1373d66a77a354ff0d8dc8853
Andreea Nechifor <Andreea.Nechifor@ness.com>
2018-08-17 11:29:01 +0300

    REPO-3654: Added the base structure for rest-api call.

---
+++ src/main/java/org/alfresco/bm/rest/CreateNodeTest.java
@@ -0,0 +1,54 @@
+/*
+ * #%L
+ * Alfresco Benchmark Manager
+ * %%
+ * Copyright (C) 2005 - 2018 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software. 
+ * If the software was purchased under a paid Alfresco license, the terms of 
+ * the paid license agreement will prevail.  Otherwise, the software is 
+ * provided under the following open source license terms:
+ * 
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.bm.rest;
+
+import org.alfresco.bm.driver.event.Event;
+import org.alfresco.rest.model.RestNodeBodyModel;
+import org.alfresco.utility.model.ContentModel;
+import org.apache.commons.lang.RandomStringUtils;
+
+public class CreateNodeTest extends RestTest {
+
+	public CreateNodeTest(String baseUrl) {
+		super(baseUrl);
+	}
+
+	@Override
+	protected void prepareData() throws Exception {
+		restClient.authenticateUser(dataContent.getAdminUser());
+	}
+
+	@Override
+	protected void restCall(Event event) throws Exception {
+		RestNodeBodyModel node = new RestNodeBodyModel();
+		node.setName(RandomStringUtils.randomAlphanumeric(8));
+		node.setNodeType("cm:folder");
+
+		restClient.withParams("autoRename=true").withCoreAPI().usingNode(ContentModel.my()).createNode(node);
+
+	}
+
+}
---
+++ src/main/java/org/alfresco/bm/rest/RestRequestsTest.java
@@ -0,0 +1,63 @@
+/*
+ * #%L
+ * Alfresco Benchmark Manager
+ * %%
+ * Copyright (C) 2005 - 2018 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software. 
+ * If the software was purchased under a paid Alfresco license, the terms of 
+ * the paid license agreement will prevail.  Otherwise, the software is 
+ * provided under the following open source license terms:
+ * 
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.bm.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.alfresco.bm.common.EventResult;
+import org.alfresco.bm.driver.event.AbstractEventProcessor;
+import org.alfresco.bm.driver.event.Event;
+
+public class RestRequestsTest extends AbstractEventProcessor {
+
+	private String eventName;
+	private int numberOfRestRequests;
+	private long creationDelay;
+
+	public RestRequestsTest(String eventName, int numberOfRestRequests, long creationDelay) {
+		this.eventName = eventName;
+		this.numberOfRestRequests = numberOfRestRequests;
+		this.creationDelay = creationDelay;
+	}
+
+	@Override
+	protected EventResult processEvent(Event event) throws Exception {
+
+		List<Event> nextEvents = new ArrayList<Event>();
+
+		long lastEventTime = System.currentTimeMillis();
+
+		for (int i = 0; i < numberOfRestRequests; i++) {
+			Event self = new Event(eventName, lastEventTime, null);
+			nextEvents.add(self);
+			lastEventTime += creationDelay;
+		}
+
+		return new EventResult(event.getName(), nextEvents);
+	}
+
+}
