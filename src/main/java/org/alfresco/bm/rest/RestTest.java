9686384aaf39341551702e6fd213e20208dc7c43
anechifor <andreea.nechifor@ness.com>
2018-08-20 19:29:54 +0300

    REPO-3654: Added the base structure for rest-api call. (#1)

---
+++ src/main/java/org/alfresco/bm/rest/RestTest.java
@@ -0,0 +1,146 @@
+/*
+ * #%L
+ * Alfresco Benchmark Rest Api
+ * %%
+ * Copyright (C) 2005 - 2018 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.bm.rest;
+
+import com.jayway.restassured.RestAssured;
+import org.alfresco.bm.common.EventResult;
+import org.alfresco.bm.driver.event.AbstractEventProcessor;
+import org.alfresco.bm.driver.event.Event;
+import org.alfresco.rest.core.RestWrapper;
+import org.alfresco.utility.data.DataContent;
+import org.alfresco.utility.data.DataSite;
+import org.alfresco.utility.data.DataUser;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.http.HttpStatus;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public abstract class RestTest extends AbstractEventProcessor implements ApplicationContextAware
+{
+
+    private ApplicationContext context;
+    protected RestWrapper restClient;
+    private String baseUrl;
+    protected DataUser dataUser;
+    protected DataContent dataContent;
+    protected DataSite dataSite;
+    protected String alfrescoAdminUsername;
+    protected String alfrescoAdminPassword;
+
+    public RestTest(String baseUrl)
+    {
+        this.baseUrl = baseUrl;
+    }
+
+    public void setAlfrescoAdminUsername(String alfrescoAdminUsername)
+    {
+        this.alfrescoAdminUsername = alfrescoAdminUsername;
+    }
+
+    public void setAlfrescoAdminPassword(String alfrescoAdminPassword)
+    {
+        this.alfrescoAdminPassword = alfrescoAdminPassword;
+    }
+
+    private void initializeRestClient() throws MalformedURLException
+    {
+        dataUser = (DataUser) this.context.getBean("dataUser");
+        restClient = (RestWrapper) this.context.getBean("restWrapper");
+        dataContent = (DataContent) this.context.getBean("dataContent");
+        dataSite = (DataSite) this.context.getBean("dataSite");
+
+        URL url = new URL(baseUrl);
+
+        RestAssured.baseURI = url.getProtocol() + "://" + url.getHost();
+        RestAssured.port = url.getPort();
+        restClient.configureRequestSpec().setBaseUri(RestAssured.baseURI).setPort(RestAssured.port);
+
+        restClient.configureRequestSpec().setBaseUri(baseUrl);
+    }
+
+    protected abstract void prepareData() throws Exception;
+
+    protected abstract void restCall(Event event) throws Exception;
+
+    @Override
+    protected EventResult processEvent(Event event) throws Exception
+    {
+
+        super.suspendTimer();
+
+        initializeRestClient();
+
+        prepareData();
+
+        super.resumeTimer();
+        restCall(event);
+        super.suspendTimer();
+
+        return processStatusCode(event.getName(), restClient.getStatusCode());
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
+    {
+        this.context = applicationContext;
+    }
+
+    private EventResult processStatusCode(String eventName, String statusCode)
+    {
+        if (HttpStatus.CREATED.toString().equals(statusCode))
+        {
+            return markAsSucces(eventName);
+        }
+        else if (HttpStatus.OK.toString().equals(statusCode))
+        {
+            return markAsSucces(eventName);
+        }
+        else if (HttpStatus.CONFLICT.toString().equals(statusCode))
+        {
+            return markAsFailure(eventName);
+        }
+        else
+        {
+            return markAsFailure(eventName);
+        }
+
+    }
+
+    private EventResult markAsFailure(String eventName)
+    {
+        return new EventResult("Event execution failed:" + eventName, false);
+    }
+
+    private EventResult markAsSucces(String eventName)
+    {
+        return new EventResult("Event execution succed:" + eventName, true);
+    }
+
+}
---
+++ src/test/java/org/alfresco/bm/ResponsivenessBMDriverTest.java
@@ -0,0 +1,171 @@
+/*
+ * #%L
+ * Alfresco Benchmark Rest Api
+ * %%
+ * Copyright (C) 2005 - 2018 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.bm;
+
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import org.alfresco.bm.common.EventRecord;
+import org.alfresco.bm.common.ResultService;
+import org.alfresco.bm.common.TestService;
+import org.alfresco.bm.common.mongo.MongoTestDAO;
+import org.alfresco.bm.common.session.SessionService;
+import org.alfresco.bm.common.spring.TestRunServicesCache;
+import org.alfresco.bm.common.util.junit.tools.BMTestRunner;
+import org.alfresco.bm.common.util.junit.tools.BMTestRunnerListener;
+import org.alfresco.bm.common.util.junit.tools.BMTestRunnerListenerAdaptor;
+import org.alfresco.bm.common.util.log.LogService;
+import org.alfresco.bm.common.util.log.LogService.LogLevel;
+import org.alfresco.bm.driver.event.Event;
+import org.alfresco.bm.manager.api.v1.ResultsRestAPI;
+import org.alfresco.bm.manager.api.v1.TestRestAPI;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(JUnit4.class)
+public class ResponsivenessBMDriverTest extends BMTestRunnerListenerAdaptor
+{
+    private static Log logger = LogFactory.getLog(ResponsivenessBMDriverTest.class);
+
+    @Test
+    public void runComplete() throws Exception
+    {
+        BMTestRunner runner = new BMTestRunner(60000L);         // Should be done in 60s
+        runner.addListener(this);
+        runner.run(null, null, null);
+    }
+
+    /**
+     * A listener method that allows the test to check results <b>before</b> the in-memory MongoDB instance
+     * is discarded.
+     * <p/>
+     * Check that the exact number of results are available, as expected
+     *
+     * @see BMTestRunnerListener
+     */
+    @Override
+    public void testRunFinished(ApplicationContext testCtx, String test, String run)
+    {
+        TestRunServicesCache services = testCtx.getBean(TestRunServicesCache.class);
+        LogService logService = testCtx.getBean(LogService.class);
+        MongoTestDAO testDAO = services.getTestDAO();
+        TestService testService = services.getTestService();
+        ResultService resultService = services.getResultService(test, run);
+        SessionService sessionService = services.getSessionService(test, run);
+        assertNotNull(resultService);
+        TestRestAPI testAPI = new TestRestAPI(testDAO, testService, logService, services);
+        ResultsRestAPI resultsAPI = new ResultsRestAPI(services);
+        // Let's check the results before the DB gets thrown away (we didn't make it ourselves)
+
+        // Dump one of each type of event for information
+        Set<String> eventNames = new TreeSet<String>(resultService.getEventNames());
+        logger.info("Showing 1 of each type of event:");
+        for (String eventName : eventNames)
+        {
+            List<EventRecord> eventRecord = resultService.getResults(eventName, 0, 1);
+            logger.info("   " + eventRecord);
+            assertFalse(
+                "An event was created that has no available processor or producer: " + eventRecord + ".  Use the TerminateEventProducer to absorb events.",
+                eventRecord.toString().contains("processedBy=unknown"));
+        }
+
+        // One successful START event
+        assertEquals("Incorrect number of start events.", 1, resultService.countResultsByEventName(Event.EVENT_NAME_START));
+        List<EventRecord> results = resultService.getResults(0L, Long.MAX_VALUE, false, 0, 1);
+        if (results.size() != 1 || !results.get(0).getEvent().getName().equals(Event.EVENT_NAME_START))
+        {
+            Assert.fail(Event.EVENT_NAME_START + " failed: \n" + results.toString());
+        }
+
+        /*
+         * 'start' = 1 result
+         * 'createNode' = 200 results
+         * Sessions = 200
+         */
+        assertEquals("Incorrect number of event names: " + eventNames, 2, eventNames.size());
+        assertEquals("Incorrect number of events: " + "createNode", 200, resultService.countResultsByEventName("createNode"));
+        // 203 events in total
+        assertEquals("Incorrect number of results.", 201, resultService.countResults());
+        // Check that we got the failure rate correct ~30%
+        long failures = resultService.countResultsByFailure();
+        assertEquals("Failure rate out of bounds. ", 200.0, (double) failures, 15.0);
+
+        // Get the summary CSV results for the time period and check some of the values
+        String summary = BMTestRunner.getResultsCSV(resultsAPI, test, run);
+        logger.info(summary);
+        assertTrue(summary.contains(",,createNode,   200,"));
+
+        // Get the chart results and check
+        String chartData = resultsAPI.getTimeSeriesResults(test, run, 0L, "seconds", 1, 10, true);
+        if (logger.isDebugEnabled())
+        {
+            logger.debug("ResponsivenessBMDriverTest chart data: \n" + chartData);
+        }
+        assertNotNull(chartData);
+        assertTrue("some values are present", chartData.contains("\"num\" : "));
+        // TODO add a valid check here for this test once REPO-3661 is done
+        //        // Check that we have 10.0 createNode events per second; across 10s, we should get 100 events
+        //        assertTrue("Expected 10 createNode events per second.", chartData.contains("\"num\" : 100 , \"numPerSec\" : 10.0"));
+
+        // Check the session data
+        assertEquals("All sessions should be closed: ", 0L, sessionService.getActiveSessionsCount());
+        // TODO see if we need to check the session for this test once REPO-3661 is done
+        // assertEquals("All sessions should be used: ", 200L, sessionService.getAllSessionsCount());
+
+        // Check the log messages
+        DBCursor logs = logService.getLogs(null, test, run, LogLevel.INFO, null, null, 0, 500);
+        try
+        {
+            assertEquals("Incorrect number of log messages for this test run. ", 6, logs.size());
+            logger.debug("Log messages for " + test + "." + "." + run);
+            while (logs.hasNext())
+            {
+                DBObject log = logs.next();
+                Date time = (Date) log.get("time");
+                String msg = (String) log.get("msg");
+                logger.debug("   " + " >>> " + time + " >>> " + msg);
+            }
+        }
+        finally
+        {
+            logs.close();
+        }
+    }
+}
