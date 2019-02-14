bf14ac9c0e74f627a98e2189f02709b2db65af5d
Cristina Diaconu <cristina.diaconu@ness.com>
2019-02-14 10:47:44 +0200

    SEARCH-1111 Add dropdown selection for SearchType

---
+++ src/main/java/org/alfresco/bm/rest/CustomSearchEventWeight.java
@@ -0,0 +1,59 @@
+package org.alfresco.bm.rest;
+
+import static org.alfresco.bm.rest.SiteContextConstants.EVENT_INSIGHT_ENGINE;
+import static org.alfresco.bm.rest.SiteContextConstants.EVENT_SEARCH_SERVICES;
+import static org.alfresco.bm.rest.SiteContextConstants.SEARCH_SERVICES_SEARCH_TYPE;
+import static org.alfresco.bm.rest.SiteContextConstants.INSIGHT_ENGINE_SEARCH_TYPE;
+
+import org.alfresco.bm.driver.event.EventWeight;
+
+/**
+ * Data representing relative weight for given event names.
+ * 
+ * @author Cristina Diaconu
+ *
+ */
+public class CustomSearchEventWeight extends EventWeight {
+
+	private String searchType;
+
+	public CustomSearchEventWeight(String eventName, double weight) {
+		super(eventName, weight);
+	}
+
+	public CustomSearchEventWeight(String eventName, String weights) {
+		super(eventName, -1.0, weights);
+	}
+
+	public void setSearchType(String searchType) {
+		this.searchType = searchType;
+	}
+
+	/**
+	 * Get the weight based on event name and search type. Return weight if and only
+	 * if we have one of these combinations: 
+	 * rest-collab.scenario.03.search with Search Type: Search Services Search 
+	 * or 
+	 * rest-collab.scenario.04.search with SearchType: Insight Engine Search
+	 * This is because scenario.03 and scenario.04 are not running on the same type.
+	 */
+	@Override
+	public double getWeight() {
+
+		if (isSearchServicesSearch() || isInsightEngineSearch()) {
+			return super.getWeight();
+		}
+		return 0;
+	}
+
+	private boolean isSearchServicesSearch() {
+		return getEventName().equalsIgnoreCase(EVENT_SEARCH_SERVICES)
+				&& searchType.equalsIgnoreCase(SEARCH_SERVICES_SEARCH_TYPE);
+	}
+
+	private boolean isInsightEngineSearch() {
+		return getEventName().equalsIgnoreCase(EVENT_INSIGHT_ENGINE)
+				&& searchType.equalsIgnoreCase(INSIGHT_ENGINE_SEARCH_TYPE);
+	}
+
+}
--- src/main/java/org/alfresco/bm/rest/SearchContent.java
+++ src/main/java/org/alfresco/bm/rest/SearchContent.java
@@ -1,5 +1,7 @@
 package org.alfresco.bm.rest;
 
+import static org.alfresco.bm.rest.SiteContextConstants.SEARCH_SERVICES_SEARCH_TYPE;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,13 +9,14 @@ import org.alfresco.bm.common.EventResult;
 import org.alfresco.bm.driver.event.Event;
 import org.alfresco.bm.user.UserData;
 import org.alfresco.bm.user.UserDataService;
-import org.alfresco.rest.core.RestWrapper;
+import org.alfresco.rest.core.RestResponse;
 import org.alfresco.rest.model.RestRequestRangesModel;
 import org.alfresco.rest.search.FacetQuery;
 import org.alfresco.rest.search.Pagination;
 import org.alfresco.rest.search.RestRequestQueryModel;
 import org.alfresco.rest.search.SearchRequest;
 import org.alfresco.rest.search.SearchResponse;
+import org.alfresco.rest.search.SearchSqlRequest;
 import org.alfresco.utility.model.UserModel;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -21,73 +24,78 @@ import org.json.JSONObject;
 import com.mongodb.BasicDBObjectBuilder;
 import com.mongodb.DBObject;
 
-
 /**
- * <b>INPUT:   </b>A site id, a random site member of the site and the text/query to search for <br/>
+ * <b>INPUT: </b>A site id, a random site member of the site and the text/query
+ * to search for <br/>
  * <b>ACTIONS: </b>Search for the text/query on the random site<br/>
- * <b>OUTPUT:  </b>List all the documents that match the search query<br/>
+ * <b>OUTPUT: </b>List all the documents that match the search query<br/>
  * 
  * @author Cristina Diaconu
  *
  */
-public class SearchContent extends RestTest 
-{
-	public static final String EVENT_SEARCH_CONTENT = "eventSearchContent";
+public class SearchContent extends RestTest {
+
+	private static final String EVENT_SEARCH_CONTENT = "eventSearchContent";
 
 	private String eventSearchContent;
-	
 	private UserDataService userDataService;
-	
+
+	// search common parameters
 	private String searchQuery;
+	private String searchType;
+
+	// Search Services parameters
 	private String searchLanguage;
 	private int maxItems;
 	private int skipCount;
-	
-	//range properties
+
+	// Search Services Range properties
 	private String rangeField;
 	private String rangeStart;
 	private String rangeStop;
 	private String rangeGap;
 	private boolean rangeHardend;
-	
+
 	private String facetQueries;
+
+	// Insight Engine Search parameters
+	private String format;
+	private boolean includeMetadata;
+	private String timezone;
+
 	
-	
-	public SearchContent() 
-	{
+	public SearchContent() {
 		this.eventSearchContent = EVENT_SEARCH_CONTENT;
 	}
 
-	public void setEventSearchContent(String eventSearchContent) 
-	{
+	public void setEventSearchContent(String eventSearchContent) {
 		this.eventSearchContent = eventSearchContent;
 	}
-	
-	public void setUserDataService(UserDataService userDataService) 
-	{
+
+	public void setUserDataService(UserDataService userDataService) {
 		this.userDataService = userDataService;
 	}
-	
-	public void setSearchQuery(String searchQuery) 
-	{
+
+	public void setSearchQuery(String searchQuery) {
 		this.searchQuery = searchQuery;
 	}
-	
-	public void setSearchLanguage(String searchLanguage) 
-	{
+
+	public void setSearchType(String searchType) {
+		this.searchType = searchType;
+	}
+
+	public void setSearchLanguage(String searchLanguage) {
 		this.searchLanguage = searchLanguage;
 	}
-	
-	public void setMaxItems(int maxItems) 
-	{
+
+	public void setMaxItems(int maxItems) {
 		this.maxItems = maxItems;
 	}
-	
-	public void setSkipCount(int skipCount) 
-	{
+
+	public void setSkipCount(int skipCount) {
 		this.skipCount = skipCount;
 	}
-	
+
 	public void setRangeField(String rangeField) {
 		this.rangeField = rangeField;
 	}
@@ -107,130 +115,172 @@ public class SearchContent extends RestTest
 	public void setRangeHardend(boolean rangeHardend) {
 		this.rangeHardend = rangeHardend;
 	}
-	
+
 	public void setFacetQueries(String facetQueries) {
 		this.facetQueries = facetQueries;
 	}
 
-	@Override
-	protected EventResult processEvent(Event event) throws Exception 
-	{
-		
-		super.suspendTimer();
-        
-        DBObject dataObj = (DBObject) event.getData();
-        if (dataObj == null)
-        {
-            throw new IllegalStateException("This processor requires data with field " + SiteContextConstants.SITE_ID);
-        }
-        
-        String site = (String) dataObj.get(SiteContextConstants.SITE_ID);
-        String member = (String) dataObj.get(SiteContextConstants.SITE_MEMBER);
-        
-        //Get the user for the selected site member
-        UserData userdata = userDataService.findUserByUsername(member);
-        if (userdata == null)
-        {
-            return new EventResult("Failure: The picked user doesn't exist anymore: " + member, false);
-        }
-        
-        UserModel userModel = buildUserModel(userdata);
-        RestWrapper restWrapper = getRestWrapper();
-        
-        super.resumeTimer();
-        
-        // build the search model 
-        SearchRequest searchRequest = buildSearchRequest();
-        
-        // do the actual search
-        SearchResponse container = restWrapper.authenticateUser(userModel).withSearchAPI().search(searchRequest);
-      
-        int count = container.getPagination() != null ? container.getPagination().getCount() : 0;
-        
-        super.suspendTimer();
-        
-        DBObject resultData = BasicDBObjectBuilder.start().add("msg", "Searched in " + site + "/documentLibrary.")
-                                                          .add("count", count).get();
+	public void setFormat(String format) {
+		this.format = format;
+	}
 
-        Event nextEvent = new Event(eventSearchContent, null);
-        return processStatusCode(resultData, getRestWrapper().getStatusCode(), nextEvent);
+	public void setIncludeMetadata(boolean includeMetadata) {
+		this.includeMetadata = includeMetadata;
 	}
-	
-	private UserModel buildUserModel(UserData userdata) 
-	{
-		UserModel userModel = new UserModel();
-        userModel.setUsername(userdata.getUsername());
-        userModel.setPassword(userdata.getPassword());
-        
-        return userModel;
+
+	public void setTimezone(String timezone) {
+		this.timezone = timezone;
 	}
-	
-	private SearchRequest buildSearchRequest() 
-	{
+
+	@Override
+	protected EventResult processEvent(Event event) throws Exception {
+
+		super.suspendTimer();
+
+		DBObject dataObj = (DBObject) event.getData();
+		if (dataObj == null) {
+			throw new IllegalStateException("This processor requires data with field " + SiteContextConstants.SITE_ID);
+		}
+
+		String site = (String) dataObj.get(SiteContextConstants.SITE_ID);
+		String member = (String) dataObj.get(SiteContextConstants.SITE_MEMBER);
+
+		// Get the user for the selected site member
+		UserData userdata = userDataService.findUserByUsername(member);
+		if (userdata == null) {
+			return new EventResult("Failure: The picked user doesn't exist anymore: " + member, false);
+		}
+
+		super.resumeTimer();
+		int count = 0;
+
+		if (searchType != null && searchType.equalsIgnoreCase(SEARCH_SERVICES_SEARCH_TYPE)) {
+			count = performSearchSearvicesSearch(userdata);
+		} else {
+			count = performInsightEngineSearch(userdata);
+		}
+
+		super.suspendTimer();
+
+		DBObject resultData = BasicDBObjectBuilder.start().add("msg", "Searched in " + site + "/documentLibrary.")
+				.add("count", count).get();
+
+		Event nextEvent = new Event(eventSearchContent, null);
+		return processStatusCode(resultData, getRestWrapper().getStatusCode(), nextEvent);
+	}
+
+	// Perform the Search using SearchServices
+	private int performSearchSearvicesSearch(UserData userdata) throws Exception {
+
+		UserModel userModel = buildUserModel(userdata);
+
+		// build the search model
+		SearchRequest searchRequest = buildSearchRequest();
+
+		// do the actual search
+		SearchResponse container = getRestWrapper().authenticateUser(userModel).withSearchAPI().search(searchRequest);
+
+		return container.getPagination() != null ? container.getPagination().getCount() : 0;
+	}
+
+	// Perform a SQL Search using Insight Engine
+	private int performInsightEngineSearch(UserData userdata) throws Exception {
+
+		UserModel userModel = buildUserModel(userdata);
+
+		// build the search model
+		SearchSqlRequest searchRequest = buildSqlSearchRequest();
+
+		// do the actual search
+		RestResponse response = getRestWrapper().authenticateUser(userModel).withSearchSqlAPI()
+				.searchSql(searchRequest);
+
+		return response.getResponse().jsonPath().get("list.pagination.totalItems");
+	}
+
+	private UserModel buildUserModel(UserData userdata) {
+
+		return new UserModel(userdata.getUsername(), userdata.getPassword());
+	}
+
+	private SearchRequest buildSearchRequest() {
+
 		RestRequestQueryModel model = new RestRequestQueryModel();
-        model.setQuery(searchQuery);
-        model.setLanguage(searchLanguage);
-        
-        SearchRequest searchRequest = new SearchRequest();
-        searchRequest.setQuery(model);
-        
-        // set pagination properties
-        Pagination pagination = new Pagination();
-        pagination.setMaxItems(maxItems);
-        pagination.setSkipCount(skipCount); 
-        searchRequest.setPaging(pagination);
-       
-        // add Range parameters
-        searchRequest.setRanges(getRanges());
-        
-        //add Facet queries 
-        List<FacetQuery> queries = parseFacetQueries(facetQueries);
-        searchRequest.setFacetQueries(queries);
-        
-        return searchRequest;
+		model.setQuery(searchQuery);
+		model.setLanguage(searchLanguage);
+
+		SearchRequest searchRequest = new SearchRequest();
+		searchRequest.setQuery(model);
+
+		// set pagination properties
+		Pagination pagination = new Pagination();
+		pagination.setMaxItems(maxItems);
+		pagination.setSkipCount(skipCount);
+		searchRequest.setPaging(pagination);
+
+		// add Range parameters
+		searchRequest.setRanges(getRanges());
+
+		// add Facet queries
+		List<FacetQuery> queries = parseFacetQueries(facetQueries);
+		searchRequest.setFacetQueries(queries);
+
+		return searchRequest;
 	}
-	
+
 	private List<RestRequestRangesModel> getRanges() {
-		
+
 		if (rangeField == null || rangeField.isEmpty()) {
 			return null;
 		}
-		
+
 		RestRequestRangesModel rangeModel = new RestRequestRangesModel();
-        rangeModel.setField(rangeField);
-        rangeModel.setStart(rangeStart);
-        rangeModel.setEnd(rangeStop);
-        rangeModel.setGap(rangeGap);
-        rangeModel.setHardend(rangeHardend);
-        
-        List<RestRequestRangesModel> ranges = new ArrayList<RestRequestRangesModel>();
-        ranges.add(rangeModel);
-        
-        return ranges;
+		rangeModel.setField(rangeField);
+		rangeModel.setStart(rangeStart);
+		rangeModel.setEnd(rangeStop);
+		rangeModel.setGap(rangeGap);
+		rangeModel.setHardend(rangeHardend);
+
+		List<RestRequestRangesModel> ranges = new ArrayList<RestRequestRangesModel>();
+		ranges.add(rangeModel);
+
+		return ranges;
 	}
-	
+
 	private List<FacetQuery> parseFacetQueries(String facets) {
-		
+
 		List<FacetQuery> facetQueries = new ArrayList<FacetQuery>();
-		
+
 		// no facet queries to process
 		if (facets == null || facets.isEmpty()) {
 			return facetQueries;
 		}
-		
+
 		JSONArray facetsJson = new JSONArray(facets);
-		for (int i = 0; i < facetsJson.length(); i++)
-        {
-            JSONObject entry = facetsJson.getJSONObject(i);
+		for (int i = 0; i < facetsJson.length(); i++) {
+			JSONObject entry = facetsJson.getJSONObject(i);
 
 			FacetQuery fQuery = new FacetQuery();
-			fQuery.setQuery( entry.get("query") == null ? "" : entry.get("query").toString());
-			fQuery.setLabel( entry.get("label") == null ? "" : entry.get("label").toString());
-			fQuery.setGroup( entry.get("group") == null ? "" : entry.get("group").toString());
+			fQuery.setQuery(entry.get("query") == null ? "" : entry.get("query").toString());
+			fQuery.setLabel(entry.get("label") == null ? "" : entry.get("label").toString());
+			fQuery.setGroup(entry.get("group") == null ? "" : entry.get("group").toString());
 			facetQueries.add(fQuery);
-        }
-		
+		}
+
 		return facetQueries;
+	}
+
+	private SearchSqlRequest buildSqlSearchRequest() {
+		
+		SearchSqlRequest searchRequest = new SearchSqlRequest();
+
+		searchRequest.setSql(searchQuery);
+		searchRequest.setFormat(format);
+		searchRequest.setIncludeMetadata(includeMetadata);
+		// locales
+		searchRequest.setTimezone(timezone);
+
+		return searchRequest;
 	}
 
 }
